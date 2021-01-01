94a8202faeb1fb1da20c5936566df9835b960f70
saeedhassansolangi <saeedhassansolangi@gmail.com>
2021-01-01 16:42:20 +0500

    removed node_modules

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,6 +1,13 @@
 const multer = require('multer');
+const datauri = require('datauri/parser');
+const path = require('path');
+
 const storage = multer.memoryStorage();
+const dUri = new datauri();
 
 const multerUploads = multer({ storage }).single('image');
 
-module.exports = multerUploads;
+const dataUri = (req) =>
+  dUri.format(path.extname(req.file.originalname).toString(), req.file.buffer);
+
+module.exports = { multerUploads, dataUri };
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const multerUploads = require('./middleware/multer');
+const { multerUploads, dataUri } = require('./middleware/multer');
+const { cloudinaryConfig, uploader } = require('./config/cloudinaryConfig');
+
 const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('*', cloudinaryConfig);
 
 app.get('/', (req, res) => {
   res.render('home.ejs');
@@ -9,6 +17,25 @@ app.get('/', (req, res) => {
 app.post('/upload', multerUploads, (req, res) => {
   console.log('req.body :', req.body);
   console.log('req.file : ', req.file);
+
+  if (req.file) {
+    const file = dataUri(req).content;
+    return uploader
+      .upload(file)
+      .then((result) => {
+        const image = result.url;
+        return res.status(200).json({
+          messge: 'Your image has been uploded successfully to cloudinary',
+          data: { image },
+        });
+      })
+      .catch((err) =>
+        res.status(400).json({
+          messge: 'someting went wrong while processing your request',
+          data: { err },
+        })
+      );
+  }
 });
 
-app.listen(3000);
+app.listen(PORT);
